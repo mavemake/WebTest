8c8b206dd7cfa79d72f611a8e4298fdc1c92b13c
Developer <developer@example.com>
2025-11-10 22:21:42 +0800

    Fix Gunicorn deployment issue by renaming app instance to application

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from app import create_app
 from config import Config
 
-app = create_app()
+application = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    application.run(debug=False)
